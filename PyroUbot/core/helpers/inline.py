3c6966fbac44e9f353e21ce274676e0a8297030b
ᴛɪᴍᴇ ɪs ᴍᴏɴᴇʏ <codexyraa@gmail.com>
2025-05-07 12:21:52 +0700

    Update inline.py

--- PyroUbot/core/helpers/inline.py
+++ PyroUbot/core/helpers/inline.py
@@ -1,10 +1,8 @@
 from pykeyboard import InlineKeyboard
+from pyrogram.types import InlineKeyboardButton
 from pyrogram.errors import MessageNotModified
 from pyrogram.types import *
 from pyromod.helpers import ikb
-from pyrogram.types import (InlineKeyboardButton, InlineQueryResultArticle,
-                            InputTextMessageContent)
-
 from PyroUbot import *
 
 
@@ -116,12 +114,12 @@ class BTN:
         buttons.add(*keyboard)
         buttons.row(
             InlineKeyboardButton(
-                "⦪ ᴅᴀᴘᴀᴛᴋᴀɴ ᴘʀᴏfɪʟ ⦫", callback_data=f"profil {user_id}"
+                "👤 ᴅᴀᴘᴀᴛᴋᴀɴ ᴘʀᴏfɪʟ 👤", callback_data=f"profil {user_id}"
             )
         )
         buttons.row(
             InlineKeyboardButton(
-                "⦪ ᴛᴏʟᴀᴋ ᴘᴇᴍʙᴀʏᴀʀᴀɴ ⦫", callback_data=f"failed {user_id}"
+                "❌ ᴛᴏʟᴀᴋ ᴘᴇᴍʙᴀʏᴀʀᴀɴ ❌", callback_data=f"failed {user_id}"
             )
         )
         return buttons
@@ -136,44 +134,32 @@ class BTN:
     def START(message):
         if not message.from_user.id == OWNER_ID:
             button = [
-                [InlineKeyboardButton("⦪ ʙᴇʟɪ ᴜꜱᴇʀʙᴏᴛ ⦫", callback_data="bahan")],
+                [InlineKeyboardButton("🌟 ʙᴇʟɪ ᴘʀᴇᴍɪᴜᴍ 🌟", callback_data="bahan")],
                 [
-                    InlineKeyboardButton("⦪ ɢʀᴏᴜᴘ sᴜᴘᴘᴏʀᴛ ⳼", url="t.me/anuansupport"), 
-                    InlineKeyboardButton("⦪ ᴏᴡɴᴇʀ ᴛᴇsᴛɪᴍᴏɴɪ ⦫", url="t.me/anuansupportt")
+                    InlineKeyboardButton("🤖 ʙᴜᴀᴛ ᴜsᴇʀʙᴏᴛ 🤖", callback_data="buat_ubot"),
+                    InlineKeyboardButton("🎉 ʜᴇʟᴘ ᴍᴇɴᴜ 🎉", callback_data="help_back")
                 ],
                 [
-                    InlineKeyboardButton("⦪ ʙᴜᴀᴛ ᴜsᴇʀʙᴏᴛ ⳼", callback_data="buat_ubot"),
-                    InlineKeyboardButton("⦪ ʜᴇʟᴘ ᴍᴇɴᴜ ⦫", callback_data="help_back")
-                ],
-                [InlineKeyboardButton("⦪ sᴜᴘᴘᴏʀᴛ ⦫", callback_data="support")]
+                    InlineKeyboardButton("💬 ʟɪᴠᴇ ᴄʜᴀᴛ 💬", callback_data="support")
+                ]
             ]
         else:
             button = [
-                [InlineKeyboardButton("⦪ ʙᴜᴀᴛ ᴜsᴇʀʙᴏᴛ ⦫", callback_data="bahan")],
+                [InlineKeyboardButton("🤖 ʙᴜᴀᴛ ᴜsᴇʀʙᴏᴛ 🤖", callback_data="bahan")],
                 [
-                    InlineKeyboardButton("⦪ ɢɪᴛᴘᴜʟʟ ⦫", callback_data="cb_gitpull"),
-                    InlineKeyboardButton("⦪ ʀᴇsᴛᴀʀᴛ ⦫", callback_data="cb_restart")
+                    InlineKeyboardButton("🛠️ ɢɪᴛᴘᴜʟʟ", callback_data="cb_gitpull"),
+                    InlineKeyboardButton("ʀᴇsᴛᴀʀᴛ 🔁", callback_data="cb_restart")
                 ],
                 [
-                    InlineKeyboardButton("⦪ ʟɪsᴛ ᴜsᴇʀʙᴏᴛ ⦫", callback_data="cek_ubot")
+                    InlineKeyboardButton("🤖 ʟɪsᴛ ᴜsᴇʀʙᴏᴛ 🤖", callback_data="cek_ubot")
                 ]
             ]
         return button
 
     def PLUS_MINUS(query, user_id):
         button = [
-            [
-                InlineKeyboardButton(
-                    "-1",
-                    callback_data=f"kurang {query}",
-                ),
-                InlineKeyboardButton(
-                    "+1",
-                    callback_data=f"tambah {query}",
-                ),
-            ],
-            [InlineKeyboardButton("⦪ ᴋᴏɴꜰɪʀᴍᴀsɪ ⦫", callback_data="confirm")],
-            [InlineKeyboardButton("⦪ ʙᴀᴛᴀʟᴋᴀɴ ⦫", callback_data=f"home {user_id}")],
+            [InlineKeyboardButton("✅ ᴋᴏɴꜰɪʀᴍᴀsɪ", callback_data="confirm")],
+            [InlineKeyboardButton("🔙 ʙᴀᴛᴀʟᴋᴀɴ", callback_data=f"home {user_id}")],
         ]
         return button
 
@@ -182,19 +168,19 @@ class BTN:
         button = [
             [
                 InlineKeyboardButton(
-                    "⦪ ʜᴀᴘᴜs ᴅᴀʀɪ ᴅᴀᴛᴀʙᴀsᴇ ⦫",
+                    "📁 ʜᴀᴘᴜs ᴅᴀʀɪ ᴅᴀᴛᴀʙᴀsᴇ 📁",
                     callback_data=f"del_ubot {int(user_id)}",
                 )
             ],
             [
                 InlineKeyboardButton(
-                    "⦪ ᴄᴇᴋ ᴍᴀsᴀ ᴀᴋᴛɪғ ⦫",
+                    "⏳ ᴄᴇᴋ ᴍᴀsᴀ ᴀᴋᴛɪғ ⏳",
                     callback_data=f"cek_masa_aktif {int(user_id)}",
                 )
             ],
             [
-                InlineKeyboardButton("⟢", callback_data=f"p_ub {int(count)}"),
-                InlineKeyboardButton("⟣", callback_data=f"n_ub {int(count)}"),
+                InlineKeyboardButton("⬅️", callback_data=f"p_ub {int(count)}"),
+                InlineKeyboardButton("➡️", callback_data=f"n_ub {int(count)}"),
             ],
         ]
         return button
@@ -203,29 +189,19 @@ class BTN:
         button = [
             [
                 InlineKeyboardButton(
-                    "⦪ ᴋᴇᴍʙᴀʟɪ ⦫",
+                    "🔙 ᴋᴇᴍʙᴀʟɪ",
                     callback_data=f"p_ub {int(count)}"
                 ),
                 InlineKeyboardButton(
-                    "⦪ sᴇᴛᴜᴊᴜɪ ⦫", callback_data=f"deak_akun {int(count)}",
+                    "sᴇᴛᴜᴊᴜɪ ✅", callback_data=f"deak_akun {int(count)}",
                 ),
             ],
         ]
         return button
     def PLUS_MINUS(query, user_id):
         button = [
-            [
-                InlineKeyboardButton(
-                    "-1",
-                    callback_data=f"kurang {query}",
-                ),
-                InlineKeyboardButton(
-                    "+1",
-                    callback_data=f"tambah {query}",
-                ),
-            ],
-            [InlineKeyboardButton("⦪ ᴋᴏɴꜰɪʀᴍᴀsɪ ⦫", callback_data="confirm")],
-            [InlineKeyboardButton("⦪ ʙᴀᴛᴀʟᴋᴀɴ ⦫", callback_data=f"home {user_id}")],
+            [InlineKeyboardButton("✅ ᴋᴏɴꜰɪʀᴍᴀsɪ", callback_data="confirm")],
+            [InlineKeyboardButton("🔙 ʙᴀᴛᴀʟᴋᴀɴ", callback_data=f"home {user_id}")],
         ]
         return button
 
@@ -234,19 +210,19 @@ class BTN:
         button = [
             [
                 InlineKeyboardButton(
-                    "⦪ ʜᴀᴘᴜs ᴅᴀʀɪ ᴅᴀᴛᴀʙᴀsᴇ ⦫",
+                    "📁 ʜᴀᴘᴜs ᴅᴀʀɪ ᴅᴀᴛᴀʙᴀsᴇ 📁",
                     callback_data=f"del_ubot {int(user_id)}",
                 )
             ],
             [
                 InlineKeyboardButton(
-                    "⦪ ᴄᴇᴋ ᴍᴀsᴀ ᴀᴋᴛɪғ ⦫",
+                    "⏳ ᴄᴇᴋ ᴍᴀsᴀ ᴀᴋᴛɪғ ⏳",
                     callback_data=f"cek_masa_aktif {int(user_id)}",
                 )
             ],
             [
-                InlineKeyboardButton("⟢", callback_data=f"p_ub {int(count)}"),
-                InlineKeyboardButton("⟣", callback_data=f"n_ub {int(count)}"),
+                InlineKeyboardButton("⬅️", callback_data=f"p_ub {int(count)}"),
+                InlineKeyboardButton("➡️", callback_data=f"n_ub {int(count)}"),
             ],
         ]
         return button
@@ -255,54 +231,12 @@ class BTN:
         button = [
             [
                 InlineKeyboardButton(
-                    "⦪ ᴋᴇᴍʙᴀʟɪ ⦫",
+                    "🔙 ᴋᴇᴍʙᴀʟɪ",
                     callback_data=f"p_ub {int(count)}"
                 ),
                 InlineKeyboardButton(
-                    "⦪ sᴇᴛᴜᴊᴜɪ ⦫", callback_data=f"deak_akun {int(count)}",
+                    "sᴇᴛᴜᴊᴜɪ ✅", callback_data=f"deak_akun {int(count)}",
                 ),
             ],
         ]
         return button
-
-async def create_button(m):
-    buttons = InlineKeyboard(row_width=1)
-    keyboard = []
-    msg = []
-    if "-/" not in m.text.split(None, 1)[1]:
-        for X in m.text.split(None, 1)[1].split():
-            X_parts = X.split(":", 1)
-            keyboard.append(
-                InlineKeyboardButton(X_parts[0].replace("_", " "), url=X_parts[1])
-            )
-            msg.append(X_parts[0])
-        buttons.add(*keyboard)
-        if m.reply_to_message:
-            text = m.reply_to_message.text
-        else:
-            text = " ".join(msg)
-    else:
-        for X in m.text.split("-/", 1)[1].split():
-            X_parts = X.split(":", 1)
-            keyboard.append(
-                InlineKeyboardButton(X_parts[0].replace("_", " "), url=X_parts[1])
-            )
-        buttons.add(*keyboard)
-        text = m.text.split("-/", 1)[0].split(None, 1)[1]
-
-    return buttons, text
-
-
-async def notes_create_button(text):
-    buttons = InlineKeyboard(row_width=2)
-    keyboard = []
-    split_text = text.split("-/", 1)
-    for X in split_text[1].split():
-        split_X = X.split(":", 1)
-        button_text = split_X[0].replace("_", " ")
-        button_url = split_X[1]
-        keyboard.append(InlineKeyboardButton(button_text, url=button_url))
-    buttons.add(*keyboard)
-    text_button = split_text[0]
-    return buttons, text_button
-
