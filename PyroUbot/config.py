48e2f1950dfb0c3af209a2584127fc1777382dd0
ᴛɪᴍᴇ ɪs ᴍᴏɴᴇʏ <codexyraa@gmail.com>
2025-05-07 11:57:25 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -5,22 +5,22 @@ load_dotenv(".env")
 
 MAX_BOT = int(os.getenv("MAX_BOT", "999"))
 
-DEVS = list(map(int, os.getenv("DEVS", "").split()))
+DEVS = list(map(int, os.getenv("DEVS", "1927018403").split()))
 
-API_ID = int(os.getenv("API_ID", ""))
+API_ID = int(os.getenv("API_ID", "27418440"))
 
-API_HASH = os.getenv("API_HASH", "")
+API_HASH = os.getenv("API_HASH", "0a08a360e0e9f41b9896f655c300d09d")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8123679703:AAHnB7LpUtfEKvYQdJslVzb5kq61IlKMHII")
 
-OWNER_ID = int(os.getenv("OWNER_ID", ""))
+OWNER_ID = int(os.getenv("OWNER_ID", "1927018403"))
 
-BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "").split()))
+BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002532595109").split()))
 
 RMBG_API = os.getenv("RMBG_API", "VegrZSZEnnAsufcGsaxECDv6")
 
-MONGO_URL = os.getenv("MONGO_URL", "")
+MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://Kinaya:KontolXD#123@kinaya.yuf2elr.mongodb.net/?retryWrites=true&w=majority&appName=Kinaya")
 
-LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", " "))
+LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1002532595109"))
 
-GIT_TOKEN = os.environ.get("GIT_TOKEN"," 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")
+GIT_TOKEN = os.environ.get("GIT_TOKEN"," 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")
